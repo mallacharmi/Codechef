518310da753ee3c1fc93448028f17e63584e531e
Mallacharmi <147468389+mallacharmi@users.noreply.github.com>
2025-12-12 19:00:51 +0530

    File updated at 12/12/2025, 7:00:51 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,9 +1,9 @@
-# Last updated: 8/20/2025, 5:44:50 PM
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}  # Stores value: index
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in hashmap:
-                return [hashmap[complement], i]
-            hashmap[num] = i
+# Last updated: 12/12/2025, 7:00:51 PM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        hashmap = {}  # Stores value: index
+4        for i, num in enumerate(nums):
+5            complement = target - num
+6            if complement in hashmap:
+7                return [hashmap[complement], i]
+8            hashmap[num] = i
