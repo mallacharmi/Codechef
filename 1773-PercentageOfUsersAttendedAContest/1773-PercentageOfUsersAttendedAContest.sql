312db350717d14294100cf38c059fdfa92809a85
Mallacharmi <147468389+mallacharmi@users.noreply.github.com>
2025-12-22 17:23:58 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,7 @@
+-- Last updated: 12/22/2025, 5:23:59 PM
+SELECT 
+    r.contest_id,
+    ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
