67c70b13e84536eb3d673f16fb90d622226337ac
Mallacharmi <147468389+mallacharmi@users.noreply.github.com>
2025-12-28 13:30:00 +0530

    File updated at 12/28/2025, 1:30:00 PM

--- 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.py
@@ -1,19 +1,10 @@
-# Last updated: 12/28/2025, 1:28:59 PM
+# Last updated: 12/28/2025, 1:30:00 PM
 1class Solution:
 2    def largestAltitude(self, gain: List[int]) -> int:
 3        prefix=[0]*(len(gain)+1)
-4        prefix[0]=0
+4        prefix[0]=gain[0]
 5        for i in range(1,len(gain)):
-6            prefix[i]=prefix[i-1]+nums[i]
+6            prefix[i]=prefix[i-1]+gain[i]
 7        return max(prefix)
-8class Solution:
-9    def largestAltitude(self, gain: List[int]) -> int:
-10        altitude = 0
-11        max_altitude = 0
-12        
-13        for g in gain:
-14            altitude += g
-15            max_altitude = max(max_altitude, altitude)
-16        
-17        return max_altitude
-18
+8
+9
